6baa30ef1311c142e9263d04d81445144a3c426b
Phoebe Lau <phoebelau7@gmail.com>
2018-01-03 23:51:47 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,7 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.boolean :family_friendly
       t.string :opening_date
       t.string :closing_date
-      t.string :long_description
+      t.string :description
     end
   end
 end
